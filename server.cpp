0c6bb8a6b6ea47ac9d97ee0afe935659da7436e2
Erik Svensson <erik.public@gmail.com>
2013-07-30 23:29:29 +0200

    Added request timer/timeout.

--- server.cpp
+++ server.cpp
@@ -31,12 +31,14 @@ int main(int argc, char** argv)
     nrf.FlushTx();
 
     Timer timer;
+    Timer requestTimer;
 
     uint8_t *tx_buf = new uint8_t [32];
     uint8_t *rx_buf = new uint8_t [32];
     memset(tx_buf, 0, 32);
     memset(rx_buf, 0, 32);
     bool more = false;
+    int64_t msecs = 0;
     while (true) {
       timer.Reset();
       nrf.PowerUp();
@@ -45,51 +47,57 @@ int main(int argc, char** argv)
 	more = false;
 	tx_buf[0] = 0x01;
 	nrf.Send(tx_buf, 32);
-	msleep(10);
+	requestTimer.Reset();
+	msleep(1);
 	nrf.Listen();
 	bool ready = false, ds = false, mr = false;
 	do {
+	  msecs = requestTimer.ElapsedMilliseconds();
 	  nrf.GetStatus(ready, ds, mr);	
-	} while (ready == false);
-	if (nrf.Receive(rx_buf, 32) >= 0) {
-	  uint8_t command = rx_buf[0];
-	  int32_t offset = 1;
-	  if (command == 0x81) {
-	    int16_t code = 0;
-	    memcpy(&code, rx_buf + offset, sizeof(int16_t));
-	    offset += sizeof(int16_t);
-	    if (code == 0) {
-	      uint64_t device = 0;
-	      memcpy(&device, rx_buf + offset, sizeof(uint64_t));
-	      offset += sizeof(uint64_t);
-	      int32_t value = 0;
-	      memcpy(&value, rx_buf + offset, sizeof(int32_t));
-	      double real_value = value / 1000000.0;
-	      printf("Device: %016llx: %f\n", device, real_value);
-	      more = true;
+	} while (ready == false && msecs < 1000);
+	if (ready) {
+	  if (nrf.Receive(rx_buf, 32) >= 0) {
+	    uint8_t command = rx_buf[0];
+	    int32_t offset = 1;
+	    if (command == 0x81) {
+	      int16_t code = 0;
+	      memcpy(&code, rx_buf + offset, sizeof(int16_t));
+	      offset += sizeof(int16_t);
+	      if (code == 0) {
+		uint64_t device = 0;
+		memcpy(&device, rx_buf + offset, sizeof(uint64_t));
+		offset += sizeof(uint64_t);
+		int32_t value = 0;
+		memcpy(&value, rx_buf + offset, sizeof(int32_t));
+		double real_value = value / 1000000.0;
+		printf("Device: %016llx: %f\n", device, real_value);
+		more = true;
+	      }
+	      else if (code == 15) {
+		printf("No more sensors\n");
+	      }
+	      else {
+		printf("Unknown code %d\n", code);
+	      }
 	    }
-	    else if (code == 15) {
-	      printf("No more sensors\n");
-	    }
-	    else {
-	      printf("Unknown code %d\n", code);
+	    else if (command == 0xff) {
+	      int16_t code = 0;
+	      memcpy(&code, rx_buf + offset, sizeof(int16_t));
+	      printf("Error: %d\n", code);
 	    }
 	  }
-	  else if (command == 0xff) {
-	    int16_t code = 0;
-	    memcpy(&code, rx_buf + offset, sizeof(int16_t));
-	    printf("Error: %d\n", code);
-	  }
+	}
+	else {
+	  printf("Timeout...\n");
 	}
 	nrf.StopListen();
 	msleep(10);
       } while (more);
       nrf.PowerDown();
-      int64_t msecs = 0;
       do {
 	msecs = timer.ElapsedMilliseconds();
 	msleep(100);
-      } while (msecs < 5000);
+      } while (msecs < 10000);
     }
   }
   return 0;
